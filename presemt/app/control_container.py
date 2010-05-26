9c90609551a6bf012fe42451705701eb59b05285
Mathieu Virbel <txprog@gmail.com>
2010-05-26 19:53:22 +0200

    presemt: add support of pen (only in live, double tap to remove all lines)

--- presemt/app/control_container.py
+++ presemt/app/control_container.py
@@ -1,6 +1,7 @@
 __all__ = ('SlideContainer', )
 
-from pymt import MTScatterPlane, set_color
+from os import path
+from pymt import MTScatterPlane, set_color, drawLine, set_brush, gx_matrix
 from array import array
 from OpenGL.GL import *
 from OpenGL.arrays import vbo
@@ -10,12 +11,16 @@ class SlideContainer(MTScatterPlane):
         super(SlideContainer, self).__init__(**kwargs)
         self.ctx = ctx
         self.vbo = vbo.VBO('', usage=GL_STREAM_DRAW, target=GL_ARRAY_BUFFER)
+        self.linepen = []
+        self.dirty = True
         self.show_grid = True
-        
+
+        self.lcount = 0
         lines = array('f')
         for i in range(-1000,1000):
             lines.extend((i*50, -50000, i*50, 50000))
             lines.extend((-50000, i*50, 50000, i*50))
+            self.lcount += 4
         self.vbo.set_array(lines.tostring())
 
     def draw(self):
@@ -26,8 +31,32 @@ class SlideContainer(MTScatterPlane):
         glEnableClientState(GL_VERTEX_ARRAY)
         with self.vbo:
             glVertexPointer(2, GL_FLOAT, 0, None)
-            glDrawArrays(GL_LINES, 0, len(self.vbo))
+            glDrawArrays(GL_LINES, 0, self.lcount)
         glDisableClientState(GL_VERTEX_ARRAY)
+
+    def on_draw(self):
+        super(SlideContainer, self).on_draw()
+        with gx_matrix:
+            glMultMatrixf(self.transform_mat)
+            glLineWidth(3)
+            set_color(0, 0, 0, .8)
+            for x in self.linepen:
+                drawLine(x)
+
+    def on_touch_down(self, touch):
+        if self.ctx.mode == 'live' and touch.device == 'wm_pen':
+            if touch.is_double_tap:
+                self.linepen = []
+                return True
+            self.linepen.append(list(self.to_local(*touch.pos)))
+            return True
+        return super(SlideContainer, self).on_touch_down(touch)
+    def on_touch_move(self, touch):
+        if self.ctx.mode == 'live' and touch.device == 'wm_pen':
+            if len(self.linepen):
+                self.linepen[-1].extend(self.to_local(*touch.pos))
+            return True
+        return super(SlideContainer, self).on_touch_move(touch)
 
     def add_widget(self, *largs):
         self.ctx.set_dirty()
